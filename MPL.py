1eb5ae8ee97b2f39be3e9a3a18f7e38d2238234f
saarst <47382261+saarst@users.noreply.github.com>
2023-01-03 17:41:06 +0200

    MPL new start

---
+++ MPL.py
@@ -0,0 +1,83 @@
+from MPL_utils import *
+
+# scikit-learn imports
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+#args:
+
+args.batch_size = 4
+args.data_dir = 'datasets/hymenoptera_data'
+args.num_workers = 4 if torch.cuda.is_available() else 0
+args.num_labels_percent = 0.1
+args.num_classes = 2
+args.num_epochs = 15            # Number of epochs to train for
+args.model_name = "vgg"         # Models to choose from [resnet, alexnet, vgg, squeezenet, densenet]
+args.feature_extract = True     # Flag for feature extracting. When False, we fine-tune the whole model,  when True we only update the reshaped layer params
+
+
+
+#code:
+image_datasets = {x: ImageFolder(os.path.join(args.data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+args.num_labeled = round(args.num_labels_percent * len(image_datasets['train']))
+labeled_idx, _ = x_u_split(args, image_datasets['train'].targets)
+image_datasets['labeled'] = Subset(image_datasets['train'],labeled_idx)
+
+dataloaders = {x: DataLoader(image_datasets[x], batch_size=args.batch_size,
+                             shuffle=True, num_workers=args.num_workers) for x in ['train', 'labeled', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'labeled', 'val']}
+class_names = image_datasets['train'].classes
+
+print(device)
+
+
+
+# Get a batch of training data
+unlabeled_set = dataloaders['train']
+
+# Initialize the model for this run
+t_model, input_size = initialize_model(args.model_name, args.num_classes, args.feature_extract, use_pretrained=True)
+s_model, input_size = initialize_model(args.model_name, args.num_classes, args.feature_extract, use_pretrained=True)
+
+
+# Print the model we just instantiated
+print(t_model)   # student has same model
+
+t_model = t_model.to(device)
+s_model = s_model.to(device)
+
+# Gather the parameters to be optimized/updated in this run. If we are
+#  fine-tuning we will be updating all parameters. However, if we are
+#  doing feature extract method, we will only update the parameters
+#  that we have just initialized, i.e. the parameters with requires_grad
+#  is True.
+t_params_to_update = extract_params_to_learn(t_model, args.feature_extract)
+s_params_to_update = extract_params_to_learn(s_model, args.feature_extract)
+
+# Observe that all parameters are being optimized
+t_optimizer = torch.optim.SGD(t_params_to_update, lr=0.001, momentum=0.9)
+s_optimizer = torch.optim.SGD(s_params_to_update, lr=0.001, momentum=0.9)
+
+# Setup the loss fn
+criterion = nn.CrossEntropyLoss()
+
+# Train and evaluate
+t_model, hist = train_model_labeled_ref(t_model, dataloaders, criterion, t_optimizer, num_epochs=args.num_epochs)
+
+#t_model, s_model, hist = train_model(args, t_model, s_model , dataloaders, criterion, t_optimizer,s_optimizer)
+
+
---
+++ MPL_utils.py
@@ -0,0 +1,243 @@
+# imports for the tutorial
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+# pytorch imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler, Subset, RandomSampler
+from torchvision.datasets import ImageFolder
+from torchvision import models, transforms
+import torchvision
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def set_parameter_requires_grad(model, feature_extracting=False):
+    # approach 1
+    if feature_extracting:
+        # frozen model
+        model.requires_grad_(False)
+    else:
+        # fine-tuning
+        model.requires_grad_(True)
+
+    # approach 2
+    if feature_extracting:
+        # frozen model
+        for param in model.parameters():
+            param.requires_grad = False
+    else:
+        # fine-tuning
+        for param in model.parameters():
+            param.requires_grad = True
+    # note: you can also mix between frozen layers and trainable layers, but you'll need a custom
+    # function that loops over the model's layers and you specify which layers are frozen.
+
+
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    fig = plt.figure(figsize=(5, 8))
+    ax = fig.add_subplot(111)
+    ax.imshow(inp)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_axis_off()
+
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0  # image size, e.g. (3, 224, 224)
+    # new method from torchvision >= 0.13
+    weights = 'DEFAULT' if use_pretrained else None
+    # to use other checkpoints than the default ones, check the model's available chekpoints here:
+    # https://pytorch.org/vision/stable/models.html
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        # new method from torchvision >= 0.13
+        model_ft = models.resnet18(weights=weights)
+        # old method for toechvision < 0.13
+        # model_ft = models.resnet18(pretrained=use_pretrained)
+
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)  # replace the last FC layer
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        # new method from torchvision >= 0.13
+        model_ft = models.alexnet(weights=weights)
+        # old method for toechvision < 0.13
+        # model_ft = models.alexnet(pretrained=use_pretrained)
+
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG16
+        """
+        # new method from torchvision >= 0.13
+        model_ft = models.vgg16(weights=weights)
+        # old method for toechvision < 0.13
+        # model_ft = models.vgg16(pretrained=use_pretrained)
+
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        # new method from torchvision >= 0.13
+        model_ft = models.squeezenet1_0(weights=weights)
+        # old method for toechvision < 0.13
+        # model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        # new method from torchvision >= 0.13
+        model_ft = models.densenet121(weights=weights)
+        # old method for toechvision < 0.13
+        # model_ft = models.densenet121(pretrained=use_pretrained)
+
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    else:
+        raise NotImplementedError
+
+    return model_ft, input_size
+
+def extract_params_to_learn(model_ft, feature_extract):
+    params_to_update = model_ft.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        for name,param in model_ft.named_parameters():
+            if param.requires_grad == True:
+                print("\t",name)
+    return params_to_update
+
+
+
+"""
+Training function
+"""
+def train_model_labeled_ref(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+class args:
+    x=1
+
+
+def x_u_split(args, labels):
+    label_per_class = args.num_labeled // args.num_classes
+    labels = np.array(labels)
+    labeled_idx = []
+    # unlabeled data: all training data
+    unlabeled_idx = np.array(range(len(labels)))
+    for i in range(args.num_classes):
+        idx = np.where(labels == i)[0]
+        idx = np.random.choice(idx, label_per_class, False)
+        labeled_idx.extend(idx)
+    labeled_idx = np.array(labeled_idx)
+    assert len(labeled_idx) == args.num_labeled
+    np.random.shuffle(labeled_idx)
+    return labeled_idx, unlabeled_idx

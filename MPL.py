db69d92dd639d20e0f3cd2711f9a76f92e2ef23b
saarst <47382261+saarst@users.noreply.github.com>
2023-01-19 19:53:23 +0200

    floor for onecycleLR

--- MPL.py
+++ MPL.py
@@ -68,8 +68,8 @@ else:
     t_optimizer = torch.optim.RAdam(t_params_to_update)
     s_optimizer = torch.optim.RAdam(s_params_to_update)
 
-    t_scheduler = torch.optim.lr_scheduler.OneCycleLR(t_optimizer, max_lr=0.01, steps_per_epoch=round(dataset_sizes['labeled'] / args.batch_size), epochs=args.num_epochs)
-    s_scheduler = torch.optim.lr_scheduler.OneCycleLR(s_optimizer, max_lr=0.01, steps_per_epoch=round(dataset_sizes['labeled'] / args.batch_size), epochs=args.num_epochs)
+    t_scheduler = torch.optim.lr_scheduler.OneCycleLR(t_optimizer, max_lr=0.01, steps_per_epoch=floor(dataset_sizes['labeled'] / args.batch_size), epochs=args.num_epochs)
+    s_scheduler = torch.optim.lr_scheduler.OneCycleLR(s_optimizer, max_lr=0.01, steps_per_epoch=floor(dataset_sizes['labeled'] / args.batch_size), epochs=args.num_epochs)
 
     # Train and evaluate
     #t_model, hist = train_model_labeled_ref(t_model, dataloaders, criterion, t_optimizer, num_epochs=args.num_epochs)
--- MPL_utils.py
+++ MPL_utils.py
@@ -15,6 +15,7 @@ import json
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from math import floor
 from torch.utils.data import DataLoader, Dataset, SubsetRandomSampler, Subset, RandomSampler
 from torchvision.datasets import ImageFolder
 from torchvision import models, transforms
